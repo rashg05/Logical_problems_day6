655252213483edc03e2cca0c4f7a17d4bec34d34
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-30 22:28:42 +0530

    Modified: prime number program

--- logicalprograms/PrimeNumber.java
+++ logicalprograms/PrimeNumber.java
@@ -6,7 +6,7 @@ public class PrimeNumber {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the no");
+		System.out.println("Enter the number");
 		int number = sc.nextInt();
 		PrimeNumber primeNumber = new PrimeNumber();
 		primeNumber.checkPrime(number);
