d3987fccd26be1d67a3a5ce4340344c35b35fdde
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-30 22:15:15 +0530

    Added: program to check prime number

---
+++ logicalprograms/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelab.javalogical.logicalprograms;
+
+import java.util.*;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the no");
+		int number = sc.nextInt();
+		PrimeNumber primeNumber = new PrimeNumber();
+		primeNumber.checkPrime(number);
+		sc.close();
+	}
+	public void checkPrime(int num) {
+		for (int i = 0; i <= num; i++) {
+			if (i == 1 || i == 2) {
+				System.out.print(i + "\t");
+			}
+			else if (i % 2 != 0) {
+				System.out.print(i + "\t");
+			}
+
+		}
+	}
+
+}
