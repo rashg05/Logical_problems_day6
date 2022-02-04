b6a3f837371dd090b9642ea0279a3c98869fc44c
rashg05 <gajra.rashmi0504@gmail.com>
2022-02-04 20:44:32 +0530

    modified: stop watch program

--- logicalprograms/StopWatch.java
+++ logicalprograms/StopWatch.java
@@ -1,37 +1,28 @@
 package com.bridgelab.javalogical.logicalprograms;
 
+import java.util.*;
+
 public class StopWatch {
-	private final long start;
-
-	public StopWatch() {
-		start = System.currentTimeMillis();
-	}
-
-	public double elapsedTime() {
-		long now = System.currentTimeMillis();
-		return (now - start) / 1000.0;
-	}
-
 	public static void main(String[] args) {
-		int n = Integer.parseInt("10");
+		double start ;
+		double stop;
+		double ElapsedTime;
+		Scanner sc = new Scanner(System.in);
 
-		// sum of square roots of integers from 1 to n using Math.sqrt(x).
-		StopWatch timer1 = new StopWatch();
-		double sum1 = 0.0;
-		for (int i = 1; i <= n; i++) {
-			sum1 += Math.sqrt(i);
-		}
-		double time1 = timer1.elapsedTime();
-		System.out.println(sum1 + " seconds\n" + time1);
+		System.out.println("Enter 0 to start the clock");
 
-		// sum of square roots of integers from 1 to n using Math.pow(x, 0.5).
-		StopWatch timer2 = new StopWatch();
-		double sum2 = 0.0;
-		for (int i = 1; i <= n; i++) {
-			sum2 += Math.pow(i, 0.5);
-		}
-		double time2 = timer2.elapsedTime();
-		System.out.println(sum2 + " seconds\n" + time2);
+		start = sc.nextDouble();
+		start = System.currentTimeMillis();
+
+		System.out.println("Enter 1 to stop the clock");
+		stop = sc.nextDouble();
+		stop = System.currentTimeMillis();
+
+		ElapsedTime = (stop - start) / 1000;
+		System.out.println("Elapsed time is " + ElapsedTime + " seconds");
+		
+		sc.close();
+
 	}
 
 }
